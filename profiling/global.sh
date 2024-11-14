d61af8e9816823d22bf3e502141f60a2da6e3d73
sbstndb <sebastienduboid4@gmail.com>
2024-11-14 15:36:36 +0100

    Add configuration script
    	-	This script aims to run profilers for a list of binaries
    	-	TODO :
    		-	Change the name !!!
    		-	launch previously added scripts, not directly binaries
    		-	Be sure creating associated folders in the right location

---
+++ profiling/global.sh
@@ -0,0 +1,24 @@
+
+
+kernel_list=(
+#	"demos/FiniteVolume/finite-volume-level-set"
+	"demos/FiniteVolume/finite-volume-advection-2d"
+	"demos/tutorial/tutorial-graduation-case-2"
+	)
+
+arg_list=(
+#	"--max-level 6"
+	"--max-level 10"
+	"--max-level 14"
+)
+
+
+# should verify if same size
+
+
+for i in "${!kernel_list[@]}"; do 
+	binary="${kernel_list[$i]}"
+	args="${arg_list[$i]}"
+	echo "Run $binary $args"
+	./$binary $args
+done

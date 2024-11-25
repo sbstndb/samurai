d170126ced4612f297b26b600c6e33db64871a24
sebastien.dubois1 <sebastiendubois4@gmail.com>
2024-11-25 23:12:47 +0100

    Add no-pie flag
            -       Due to spack which compile cli11 as static,
                    I have to compile with no-pie. (position independant execution)

--- compile_commands.sh
+++ compile_commands.sh
@@ -15,11 +15,11 @@ cmake -B build_gcc_omp -S . -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_FLAGS="-mtune
 
 # 2) with LLVM
 
-cmake -B build_clang_O2 -S . -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_FLAGS="-mtune=native -march=native -O2" -DCMAKE_CXX_COMPILER=clang++ -DBUILD_DEMOS=ON -DCMAKE_EXE_LINKER_FLAGS="-pie"
-cmake -B build_clang_O3 -S . -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_FLAGS="-mtune=native -march=native -O3" -DCMAKE_CXX_COMPILER=clang++ -DBUILD_DEMOS=ON -DCMAKE_EXE_LINKER_FLAGS="-pie"
-cmake -B build_clang_Ofast -S . -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_FLAGS="-mtune=native -march=native -Ofast" -DCMAKE_CXX_COMPILER=clang++ -DBUILD_DEMOS=ON -DCMAKE_EXE_LINKER_FLAGS="-pie"
-cmake -B build_clang_without_native -S . -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_FLAGS="-Ofast" -DCMAKE_CXX_COMPILER=clang++ -DBUILD_DEMOS=ON -DCMAKE_EXE_LINKER_FLAGS="-pie"
-cmake -B build_clang_full -S . -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_FLAGS="-mtune=native -march=native -Ofast -mavx2 -funroll-loops -fpeel-loops -ftree-vectorize" -DCMAKE_CXX_COMPILER=clang++ -DBUILD_DEMOS=ON -DCMAKE_EXE_LINKER_FLAGS="-pie"
-cmake -B build_clang_xsimd -S . -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_FLAGS="-mtune=native -march=native -Ofast -mavx2 -funroll-loops -fpeel-loops -ftree-vectorize" -DCMAKE_CXX_COMPILER=clang++ -DBUILD_DEMOS=ON -DXTENSOR_USE_XSIMD=ON -DCMAKE_EXE_LINKER_FLAGS="-pie"
-cmake -B build_clang_omp -S . -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_FLAGS="-mtune=native -march=native -Ofast -mavx2 -funroll-loops -fpeel-loops -ftree-vectorize" -DCMAKE_CXX_COMPILER=clang++ -DBUILD_DEMOS=ON -DWITH_OPENMP=ON -DCMAKE_EXE_LINKER_FLAGS="-pie"
+cmake -B build_clang_O2 -S . -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_FLAGS="-mtune=native -march=native -O2" -DCMAKE_CXX_COMPILER=clang++ -DBUILD_DEMOS=ON -DCMAKE_EXE_LINKER_FLAGS="-no-pie"
+cmake -B build_clang_O3 -S . -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_FLAGS="-mtune=native -march=native -O3" -DCMAKE_CXX_COMPILER=clang++ -DBUILD_DEMOS=ON -DCMAKE_EXE_LINKER_FLAGS="-no-pie"
+cmake -B build_clang_Ofast -S . -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_FLAGS="-mtune=native -march=native -Ofast" -DCMAKE_CXX_COMPILER=clang++ -DBUILD_DEMOS=ON -DCMAKE_EXE_LINKER_FLAGS="-no-pie"
+cmake -B build_clang_without_native -S . -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_FLAGS="-Ofast" -DCMAKE_CXX_COMPILER=clang++ -DBUILD_DEMOS=ON -DCMAKE_EXE_LINKER_FLAGS="-no-pie"
+cmake -B build_clang_full -S . -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_FLAGS="-mtune=native -march=native -Ofast -mavx2 -funroll-loops -fpeel-loops -ftree-vectorize" -DCMAKE_CXX_COMPILER=clang++ -DBUILD_DEMOS=ON -DCMAKE_EXE_LINKER_FLAGS="-no-pie"
+cmake -B build_clang_xsimd -S . -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_FLAGS="-mtune=native -march=native -Ofast -mavx2 -funroll-loops -fpeel-loops -ftree-vectorize" -DCMAKE_CXX_COMPILER=clang++ -DBUILD_DEMOS=ON -DXTENSOR_USE_XSIMD=ON -DCMAKE_EXE_LINKER_FLAGS="-no-pie"
+cmake -B build_clang_omp -S . -DCMAKE_BUILD_TYPE="Debug" -DCMAKE_CXX_FLAGS="-mtune=native -march=native -Ofast -mavx2 -funroll-loops -fpeel-loops -ftree-vectorize" -DCMAKE_CXX_COMPILER=clang++ -DBUILD_DEMOS=ON -DWITH_OPENMP=ON -DCMAKE_EXE_LINKER_FLAGS="-no-pie"
 

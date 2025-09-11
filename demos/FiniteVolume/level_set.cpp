56ffba402046c9c483353dbbe2748427df5b3ae3
Ubuntu <sebastiendubois4@gmail.com>
2025-09-11 17:49:17 +0000

    feat: use samurai wrapper print for demos

--- demos/FiniteVolume/level_set.cpp
+++ demos/FiniteVolume/level_set.cpp
@@ -349,7 +349,7 @@ int main(int argc, char* argv[])
             t = Tf;
         }
 
-        samurai::io::print("iteration {}: t = {}, dt = {}\n", nt++, t, dt);
+        samurai::io::print(samurai::io::root, "iteration {}: t = {}, dt = {}\n", nt++, t, dt);
 
         // Numerical scheme
         samurai::update_ghost(phi, u);

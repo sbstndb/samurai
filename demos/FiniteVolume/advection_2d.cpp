e794e2878144dbf4672601d88cad02a5a8391d4b
Ubuntu <sebastiendubois4@gmail.com>
2025-09-11 17:49:17 +0000

    feat: use samurai wrapper print for demos

--- demos/FiniteVolume/advection_2d.cpp
+++ demos/FiniteVolume/advection_2d.cpp
@@ -139,7 +139,7 @@ int main(int argc, char* argv[])
             t = Tf;
         }
 
-        samurai::io::print("iteration {}: t = {}, dt = {}\n", nt++, t, dt);
+        samurai::io::print(samurai::io::root, "iteration {}: t = {}, dt = {}\n", nt++, t, dt);
 
         samurai::update_ghost_mr(u);
         unp1.resize();

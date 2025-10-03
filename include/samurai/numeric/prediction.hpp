7cab315b17d097c126c8f7662d6b140b46e0f10f
sbstndb/sbstndbs <sebastiendubois4@gmail.com>
2025-10-03 11:13:19 +0200

    Revert "revert: keep std::cerr unchanged; convert only std::cout. Revert unintended eprint changes and stray includes in FV headers (part 1)"
    This reverts commit 4fbdc57b867bb94bb21e6fc7a292a74632dae752.

--- include/samurai/numeric/prediction.hpp
+++ include/samurai/numeric/prediction.hpp
@@ -8,8 +8,6 @@
 
 #include <xtensor/xadapt.hpp>
 #include <xtensor/xview.hpp>
-#include <fmt/ostream.h>
-#include "../print.hpp"
 
 #include "../operators_base.hpp"
 #include "../storage/utils.hpp"
@@ -578,8 +576,8 @@ namespace samurai
 #ifdef SAMURAI_CHECK_NAN
         if (xt::any(xt::isnan(qs_ij)))
         {
-            samurai::io::eprint("NaN detected in the prediction stencil (Qs_ij).\n{}
-", fmt::streamed(qs_ij));
+            std::cerr << "NaN detected in the prediction stencil (Qs_ij)." << std::endl;
+            std::cerr << qs_ij << std::endl;
             save(fs::current_path(), "check_nan", {true, true}, src.mesh(), src);
         }
 #endif
